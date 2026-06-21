98b30ab98ce6168ee8f7262161f02f349c6d260e
gaurav-08-patel <gauravpatelrounak@gmail.com>
2026-06-21 21:35:52 +0545

    add Google AdSense script integration to custom document component

--- pages/_document.js
+++ pages/_document.js
@@ -1,9 +1,17 @@
 import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
+  const adsenseClientId = process.env.NEXT_PUBLIC_ADSENSE_CLIENT_ID;
+
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <script
+          async
+          src={`https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=${adsenseClientId}`}
+          crossOrigin='anonymous'
+        />
+      </Head>
       <body className="antialiased">
         <Main />
         <NextScript />
