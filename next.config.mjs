ec9a4cd4bdfd44de8dd6c49d17dbfb12112861b9
gaurav-08-patel <gauravpatelrounak@gmail.com>
2026-06-21 19:10:00 +0545

    configured next.config.js

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,23 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  /* config options here */
   reactCompiler: true,
   reactStrictMode: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'cdn.sanity.io',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'images.unsplash.com',
+        port: '',
+        pathname: '/**',
+      },
+    ],
+  },
 };
 
 export default nextConfig;
