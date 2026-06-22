221d80829f1fe2a4270df08dea2dcd98f163a2d7
gaurav-08-patel <gauravpatelrounak@gmail.com>
2026-06-23 00:35:44 +0545

    updated schema to have customCOde blcok and chips

--- src/sanity/schemaTypes/article.js
+++ src/sanity/schemaTypes/article.js
@@ -1,53 +1,59 @@
 export default {
-  name: 'article',
-  title: 'Article',
-  type: 'document',
-  fields: [
-    {
-      name: 'title',
-      type: 'string',
-      title: 'Title',
-      validation: R => R.required()
-    },
-    {
-      name: 'slug',
-      type: 'slug',
-      title: 'Slug',
-      options: { source: 'title', maxLength: 96 },
-      validation: R => R.required()
-    },
-    {
-      name: 'brief',
-      type: 'text',
-      title: 'Brief description',
-      rows: 3
-    },
-    {
-      name: 'body',
-      type: 'array',
-      title: 'Article body',
-      of: [
-        { type: 'block' },
-        { type: 'image', options: { hotspot: true } }
-      ]
-    },
-    {
-      name: 'coverImage',
-      type: 'image',
-      title: 'Cover image',
-      options: { hotspot: true }
-    },
-    {
-      name: 'tags',
-      type: 'array',
-      title: 'Tags',
-      of: [{ type: 'string' }],
-      options: { layout: 'tags' }
-    },
-    {
-      name: 'publishedAt',
-      type: 'datetime',
-      title: 'Published at'
-    },
-  ]
-}
+    name: "article",
+    title: "Article",
+    type: "document",
+    fields: [
+        {
+            name: "title",
+            type: "string",
+            title: "Title",
+            validation: (R) => R.required(),
+        },
+        {
+            name: "slug",
+            type: "slug",
+            title: "Slug",
+            options: { source: "title", maxLength: 96 },
+            validation: (R) => R.required(),
+        },
+        {
+            name: "brief",
+            type: "text",
+            title: "Brief description",
+            rows: 3,
+        },
+        {
+            name: "body",
+            type: "array",
+            title: "Article body",
+            of: [
+                {
+                    type: "block",
+                    marks: {
+                        annotations: [{ name: "chip", type: "chip" }],
+                    },
+                },
+                { type: "image", options: { hotspot: true } },
+                { type: "codeBlock" },
+            ],
+        },
+        {
+            name: "coverImage",
+            type: "image",
+            title: "Cover image",
+            options: { hotspot: true },
+        },
+        {
+            name: "tags",
+            type: "array",
+            title: "Tags",
+            of: [{ type: "string" }],
+            options: { layout: "tags" },
+        },
+        {
+            name: "publishedAt",
+            type: "datetime",
+            title: "Published at",
+        },
+    ],
+};
