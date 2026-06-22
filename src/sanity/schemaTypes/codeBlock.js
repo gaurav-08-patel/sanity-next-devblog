682fa5556c20e1f86e72f77b038d40ff41b457ee
gaurav-08-patel <gauravpatelrounak@gmail.com>
2026-06-23 00:36:28 +0545

    codeBlock schema

---
+++ src/sanity/schemaTypes/codeBlock.js
@@ -0,0 +1,22 @@
+export default {
+    name: "codeBlock",
+    title: "Code block",
+    type: "object",
+    fields: [
+        {
+            name: "language",
+            title: "Language",
+            type: "string",
+        },
+        {
+            name: "code",
+            title: "Code",
+            type: "text",
+        },
+        {
+            name: "filename",
+            title: "Filename",
+            type: "string",
+        },
+    ],
+};
